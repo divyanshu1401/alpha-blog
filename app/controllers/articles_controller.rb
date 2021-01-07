7100c6de1d35579eb6097b8692bf49923881d0a6
Divyanshu Sharma <divyanshu.sharma@cerner.com>
2021-01-07 03:27:16 +0000

    Add create route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,4 +10,14 @@ class ArticlesController < ApplicationController
 
   def about
   end
+
+  def new
+
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to @article
+  end
 end

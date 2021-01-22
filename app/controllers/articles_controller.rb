60a5fcd658001237b0e4bf2a3e3c4bbe94ac8121
Divyanshu Sharma <divyanshu.sharma@cerner.com>
2021-01-22 03:14:17 +0000

    All CRUD operations, DRY and partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,23 +1,56 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @article = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
-  def about
-  end
+  def about; end
 
   def new
-
+    @article = Article.new
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to @article
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = 'Article created successfully'
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = 'Article updated successfully'
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @article.delete
+      flash[:notice] = 'Article deleted successfully'
+      redirect_to articles_path
+    end
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end

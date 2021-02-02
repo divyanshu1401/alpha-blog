d94297765b18855af89d3414efb64612e4593fa8
Divyanshu Sharma <divyanshu.sharma@cerner.com>
2021-02-02 03:17:40 +0000

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: {minimum: 6, maximum: 100}
-  validates :email, presence: true, length: {minimum: 6, maximum: 100}
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 3, maximum: 25}
+  VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :email, presence: true, uniqueness: true, length: {maximum: 100},
+            format: { with: VALID_EMAIL_REGEX},
+            uniqueness: { case_sensitive: false }
 end

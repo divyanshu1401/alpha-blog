77b9e84c644ef94ce810fcedc5e1374fa3607838
Divyanshu Sharma <divyanshu.sharma@cerner.com>
2021-02-07 09:04:55 +0000

    Add users create action

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_032000) do
+ActiveRecord::Schema.define(version: 2021_02_03_033219) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_02_02_032000) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
